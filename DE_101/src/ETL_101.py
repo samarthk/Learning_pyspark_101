623e6e89d012298781218009e07f7fdf0ea5a9ea
Samarth <samarthcse@gmail.com>
2020-12-21 00:15:18 +0100

    check DB

--- DE_101/src/ETL_101.py
+++ DE_101/src/ETL_101.py
@@ -7,7 +7,26 @@ spark = SparkSession.builder.master("local[*]").getOrCreate()
 fireIncidentsDF = spark.read.csv('/home/sammy/Learning_pyspark/InDir/Fire_Incidents.csv', header=True, inferSchema=True)
 fireIncidentsDF.printSchema()
 fireIncidentsDF.show(5)
+print(fireIncidentsDF.count())
 
-fireIncidentsDF.write.jdbc(url ="jdbc:sqlite:/home/sammy/Learning_pyspark/SQLITE/STG.db",table="STG_FireIncidents",mode="append")
+filter_DF = fireIncidentsDF.filter(fireIncidentsDF.Incident_Number ==18066225 )
 
-print('Done !!')
+sel_DF=fireIncidentsDF.select("Address")
+
+print(filter_DF.count())
+
+try:
+    out_DF = filter_DF.write.jdbc(url ="jdbc:sqlite:/home/sammy/Learning_pyspark/SQLITE/STG.db",table="STG_FireIncidents",mode="append")
+
+except Exception as e:
+        print (" ### Exception ###: ", e)
+
+#out_DF = fireIncidentsDF.write.jdbc(url ="jdbc:sqlite:/home/sammy/Learning_pyspark/SQLITE/STG.db",isolationLevel=1,table="STG_FireIncidents",mode="append")
+
+#print('Done !!')
+
+
+#print(out_DF)
+
+sel_DF.write.jdbc(url ="jdbc:sqlite:/home/sammy/Learning_pyspark/SQLITE/STG.db",table="STG_FireIncidents",mode="append")
+
